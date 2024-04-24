d0647bb85f9c74cb73478e5943c8911f0defcd3e
Ats Art <ats.art.ilves@tktk.ee>
2024-04-24 08:42:51 +0300

    add week 3 code

---
+++ README.md
@@ -0,0 +1,4 @@
+# RKE132-Week-3
+# RKE132-Week-3
+# RKE132-Week-3
+# RKE132-Week-3-
---
+++ Switchloogika/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+Console.WriteLine("Mis hinde said?");
+
+
+
+ string hinne = Console.ReadLine().ToUpper();
+
+switch (hinne)
+
+{
+    case "A":
+
+    Console.WriteLine("Suurepärane!");
+        break;
+
+    case "B":
+
+        Console.WriteLine("Väga hea!");
+        break;
+    case "C":
+
+        Console.WriteLine("Hea!");
+        break;
+    case "D":
+
+        Console.WriteLine("Rahuldav");
+        break;
+
+     case "E":
+
+        Console.WriteLine("Kasin");
+
+    case "F":
+
+        Console.WriteLine("Puudulik");
+        break;
+
+    default
+
+        Console.WriteLine("Vale väärtus");
+    }
+break;
